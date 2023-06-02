0c1c7d2f8cab8f73088eecf325f13412d51a8956
Hannah Cherba <hannahpond@yahoo.com>
2023-06-02 11:03:25 -0700

    added the slider

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
 // ==================
-// JS Slider
+// Swiper JS
 // ==================
-
-const carousel = document.querySelector('.carousel-main');
-const leftArrow = document.querySelector('.arrow.left');
-const rightArrow = document.querySelector('.arrow.right');
-
-let translateX = 0;
-
-leftArrow.addEventListener('click', () => {
-  if (translateX < 0) {
-    translateX += carousel.offsetWidth / Math.floor(carousel.offsetWidth / carousel.firstElementChild.offsetWidth);
-    carousel.style.transform = `translateX(${translateX}px)`;
-  }
+let swiper = new Swiper(".slide-content", {
+    slidesPerView:3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor: 'true',
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
 });
 
-rightArrow.addEventListener('click', () => {
-  if (translateX > -(carousel.scrollWidth - carousel.offsetWidth)) {
-    translateX -= carousel.offsetWidth / Math.floor(carousel.offsetWidth / carousel.firstElementChild.offsetWidth);
-    carousel.style.transform = `translateX(${translateX}px)`;
-  }
+
+/*-----------------------------
+The JS Search
+------------------------------*/
+
+//Create an input variable
+
+const input = document.getElementById('search');
+
+//listen for search event
+
+input.addEventListener('keyup', e => {
+    let currentValue= e.target.value.toLowerCase();
+    let captions= document.querySelectorAll("h2");
+    captions.forEach(caption => {
+        if (caption.textContent.toLowerCase().includes(currentValue)) {
+            caption.parentNode.parentNode.style.display = 'block';
+        }else {
+            caption.parentNode.parentNode.style.display = 'none';
+        }
+    });
+
 });
